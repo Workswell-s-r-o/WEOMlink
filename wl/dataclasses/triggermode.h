30f5b688a38915221629706b4ce3a1d78a0fcfa3
Michal Peterka <michal.peterka@workswell.cz>
2026-02-13 09:07:39 +0100

    add new registers

--- wl/dataclasses/triggermode.h
+++ wl/dataclasses/triggermode.h
@@ -3,24 +3,36 @@
 
 #include <etl/enum_type.h>
 
+#include <cstdint>
+
 namespace wl {
 
+/**
+ * @struct TriggerMode
+ * @headerfile triggermode.h "wl/dataclasses/triggermode.h"
+ * @brief Defines trigger modes
+ */
 struct TriggerMode
 {
-    enum enum_type : uint32_t
+    /**
+     * @enum enum_type
+     * @brief Enum type representing the trigger modes
+     */
+    enum enum_type: uint8_t
     {
         INTERNAL_TRIGGER = 0,
         EXTERNAL_CONTINUOUS_TRIGGER = 1,
-        EXTERNAL_ONE_SHOT_TRIGGER = 2,
+        EXTERNAL_ONE_SHOT_TRIGGER = 2
     };
-
-    ETL_DECLARE_ENUM_TYPE(TriggerMode, uint32_t)
+    
+    ETL_DECLARE_ENUM_TYPE(TriggerMode, uint8_t)
     ETL_ENUM_TYPE(INTERNAL_TRIGGER, "INTERNAL_TRIGGER")
     ETL_ENUM_TYPE(EXTERNAL_CONTINUOUS_TRIGGER, "EXTERNAL_CONTINUOUS_TRIGGER")
-    ETL_ENUM_TYPE(EXTERNAL_ONE_SHOT_TRIGGER, "EXTERNAL_ONE_SHOT_TRIGGER")
+    ETL_ENUM_TYPE(EXTERNAL_ONE_SHOT_TRIGGER,  "EXTERNAL_ONE_SHOT_TRIGGER")
     ETL_END_ENUM_TYPE
 };
 
 } // namespace wl
+
 
 #endif // WL_TRIGGERMODE_H
--- wl/misc/endian.h
+++ wl/misc/endian.h
@@ -2,6 +2,9 @@
 #define WL_ENDIAN_H
 
 #include <etl/endianness.h>
+#include <etl/span.h>
+
+#include <cstring>
 
 namespace wl {
 
@@ -55,6 +58,24 @@ constexpr typename etl::enable_if<etl::is_integral<T>::value, T>::type toBigEndi
     {
         return value;
     }
+}
+
+template <typename T>
+    requires std::is_trivially_copyable_v<T>
+void serialize(T value, uint8_t* data, size_t size, size_t offset = 0)
+{
+    assert(offset + size <= sizeof(T));
+    std::memcpy(data, reinterpret_cast<const uint8_t*>(&value) + offset, size);
+}
+
+template <typename T>
+    requires std::is_trivially_copyable_v<T>
+T deserialize(const etl::span<uint8_t>& data)
+{
+    assert(data.size() >= sizeof(T));
+    T value = {};
+    std::memcpy(&value, data.data(), sizeof(T));
+    return value;
 }
 
 } // namespace wl
